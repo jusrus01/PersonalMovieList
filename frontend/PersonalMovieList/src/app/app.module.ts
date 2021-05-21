1a59a71cb689f903dea5a3244e37a79a8c8f4c2c
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-21 19:17:18 +0300

    created basic components

--- frontend/PersonalMovieList/src/app/app.module.ts
+++ frontend/PersonalMovieList/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
