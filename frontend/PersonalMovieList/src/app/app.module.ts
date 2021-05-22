18404aaa85dc39339434d23302763ab78e586248
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-22 22:31:07 +0300

    added routing

--- frontend/PersonalMovieList/src/app/app.module.ts
+++ frontend/PersonalMovieList/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "login",
+        component: LoginComponent
+
+      },
+      {
+        path: "register",
+        component: RegisterComponent
+      },
+      {
+        path: "",
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
