d028876535f7d8d8919af515d676a7919c181da2
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-22 22:57:57 +0300

    created mock movies for testing

---
+++ frontend/PersonalMovieList/src/app/mock/mock-movies.ts
@@ -0,0 +1,10 @@
+import { Movie } from '../movies/movie';
+
+export const MOVIES: Movie[] = [
+  { title: 'fu', comment: 'f', rating: 1 },
+  { title: 'fu', comment: 'fc', rating: 1 },
+  { title: 'fu', comment: 'f', rating: 1 },
+  { title: 'fu', comment: 'fc', rating: 1 },
+  { title: 'fu', comment: 'fc', rating: 1 },
+  { title: 'fu', comment: 'fc', rating: 1 }
+];
