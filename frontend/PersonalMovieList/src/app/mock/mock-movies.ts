f2a75c4a6c0f85ab5539f7fb3f19aad853ef76f9
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-25 16:57:19 +0300

    changed Movie interface and edited mock movies

--- frontend/PersonalMovieList/src/app/mock/mock-movies.ts
+++ frontend/PersonalMovieList/src/app/mock/mock-movies.ts
@@ -1,8 +1,8 @@
 import { Movie } from '../movies/movie';
 
 export const MOVIES: Movie[] = [
-  { title: 'Sherlock', comment: 'I liked it.', rating: 10 },
-  { title: 'Iron Man', comment: "It's good", rating: 6 },
-  { title: 'Iron Man II', comment: 'Nice.', rating: 4 },
-  { title: 'Iron Man III', comment: 'Very good.', rating: 10 }
+  { id: 0, title: 'Sherlock', comment: 'I liked it.', rating: 10 },
+  { id: 1, title: 'Iron Man', comment: "Its good", rating: 6 },
+  { id: 2, title: 'Iron Man II', comment: 'Nice.', rating: 4 },
+  { id: 3, title: 'Iron Man III', comment: 'Very good.', rating: 10 }
 ];
--- frontend/PersonalMovieList/src/app/movies/movie.ts
+++ frontend/PersonalMovieList/src/app/movies/movie.ts
@@ -1,4 +1,5 @@
 export interface Movie {
+    id: number;
     title: string;
     comment: string;
     rating: number;
