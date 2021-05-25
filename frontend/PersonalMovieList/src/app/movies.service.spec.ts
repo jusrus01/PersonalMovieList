3e656a0fe30587f1a199e43813d838306af26694
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-25 17:21:21 +0300

    created a unit test for service remove method

--- frontend/PersonalMovieList/src/app/movies.service.spec.ts
+++ frontend/PersonalMovieList/src/app/movies.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { MOVIES } from './mock/mock-movies';
 
 import { MoviesService } from './movies.service';
+import { Movie } from './movies/movie';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -13,4 +15,22 @@ describe('MoviesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('deletion function', () => {
+
+    let movies : Movie[];
+    let service : MoviesService;
+    
+    beforeEach(() => {
+      service = new MoviesService();
+    });
+
+    it('should remove movie from the array', () => {
+      const movies = service.fetchMovies();
+      const removedMovie = movies[0];
+      service.removeMovie(removedMovie);
+      expect(service.movies.includes(removedMovie)).toBeFalse();
+    });
+  })
+
 });
