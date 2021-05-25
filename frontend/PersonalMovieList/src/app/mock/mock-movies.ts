9516410cd665e7eaa5c50cb82ae16560872f97b1
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-25 13:51:59 +0300

    changed mock data to more realistic data

--- frontend/PersonalMovieList/src/app/mock/mock-movies.ts
+++ frontend/PersonalMovieList/src/app/mock/mock-movies.ts
@@ -1,10 +1,8 @@
 import { Movie } from '../movies/movie';
 
 export const MOVIES: Movie[] = [
-  { title: 'fu', comment: 'f', rating: 1 },
-  { title: 'fu', comment: 'fc', rating: 1 },
-  { title: 'fu', comment: 'f', rating: 1 },
-  { title: 'fu', comment: 'fc', rating: 1 },
-  { title: 'fu', comment: 'fc', rating: 1 },
-  { title: 'fu', comment: 'fc', rating: 1 }
+  { title: 'Sherlock', comment: 'I liked it.', rating: 10 },
+  { title: 'Iron Man', comment: "It's good", rating: 6 },
+  { title: 'Iron Man II', comment: 'Nice.', rating: 4 },
+  { title: 'Iron Man III', comment: 'Very good.', rating: 10 }
 ];
