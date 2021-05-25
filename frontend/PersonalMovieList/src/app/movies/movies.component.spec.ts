25b98168a5e9758733f57866be8795c187f73433
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-25 13:51:35 +0300

    added more tests for movies component

--- frontend/PersonalMovieList/src/app/movies/movies.component.spec.ts
+++ frontend/PersonalMovieList/src/app/movies/movies.component.spec.ts
@@ -22,4 +22,37 @@ describe('MoviesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('movies component should have title', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-title"]')).toBeTruthy();
+  });
+
+  it('movies component should have comment', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-comment"]')).toBeTruthy();
+  });
+
+  it('movies component should have rating', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-rating"]')).toBeTruthy();
+  });
+
+  it('movies component should have remove button', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-remove"]')).toBeTruthy();
+  });
+
+  it('movies component should have add button', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-add"]')).toBeTruthy();
+  });
+
+  it('movies component should have edit button', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-edit"]')).toBeTruthy();
+  });
+
+  it('movies component should have sort option', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-sort"]')).toBeTruthy();
+  });
+
+  it('movies component should have image', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="movie-image"]')).toBeTruthy();
+  });
+
 });
