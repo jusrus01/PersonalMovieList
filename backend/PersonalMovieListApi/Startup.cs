803550277069b0637aca02453d432dc0938d30fc
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-27 20:54:16 +0300

    configured api

--- backend/PersonalMovieListApi/Startup.cs
+++ backend/PersonalMovieListApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PersonalMovieListApi.Data;
 
 namespace PersonalMovieListApi
 {
@@ -23,25 +24,17 @@ namespace PersonalMovieListApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PersonalMovieListApi", Version = "v1" });
-            });
+            services.AddScoped<IMoviesRepo, MockMoviesRepo>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PersonalMovieListApi v1"));
             }
 
             app.UseHttpsRedirection();
