c5dbcdbc28c5450b6e177f4935735a4db180568f
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-27 18:54:19 +0300

    wrote basic unit tests

--- frontend/PersonalMovieList/src/app/movies/movie-create-modal/movie-create-modal.component.spec.ts
+++ frontend/PersonalMovieList/src/app/movies/movie-create-modal/movie-create-modal.component.spec.ts
@@ -22,4 +22,26 @@ describe('MovieCreateModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
+  it('create modal component should have title field', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="create-title"]')).toBeTruthy();
+  });
+    
+  it('create modal component should have image field', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="create-image"]')).toBeTruthy();
+  });
+    
+  it('create modal component should have comment field', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="create-comment"]')).toBeTruthy();
+  });
+    
+  it('create modal component should have rating field', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="create-rating"]')).toBeTruthy();
+  });
+    
+  it('create modal component should have create button', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="create-button"]')).toBeTruthy();
+  });
+
 });
