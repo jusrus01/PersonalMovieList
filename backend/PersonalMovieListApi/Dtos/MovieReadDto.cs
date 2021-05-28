f122f3b1fe61476bcd3a147da3ed497f380ea0a7
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-28 14:05:09 +0300

    Created read movie dto

---
+++ backend/PersonalMovieListApi/Dtos/MovieReadDto.cs
@@ -0,0 +1,9 @@
+namespace PersonalMovieListApi.Dtos
+{
+    public class MovieReadDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+    }
+}
