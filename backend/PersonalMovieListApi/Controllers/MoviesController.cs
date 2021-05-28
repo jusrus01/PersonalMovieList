fb3e40346e7b50ff28dacfc5fdf0522f65c377d0
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-28 14:29:43 +0300

    Modifed controller to use Dto

--- backend/PersonalMovieListApi/Controllers/MoviesController.cs
+++ backend/PersonalMovieListApi/Controllers/MoviesController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PersonalMovieListApi.Data;
+using PersonalMovieListApi.Dtos;
 using PersonalMovieListApi.Models;
 
 namespace PersonalMovieListApi.Controllers
@@ -10,17 +12,19 @@ namespace PersonalMovieListApi.Controllers
     public class MoviesController : Controller
     {
         private readonly IMoviesRepo _repo;
+        private IMapper _mapper;
 
-        public MoviesController(IMoviesRepo repo)
+        public MoviesController(IMoviesRepo repo, IMapper mapper)
         {
             _repo = repo;
+            _mapper = mapper;
         }
         
         [HttpGet]
         public ActionResult <IEnumerable<Movie>> GetAllMovies()
         {
             IEnumerable<Movie> movies = _repo.GetAllMovies();
-            return Ok(movies);
+            return Ok(_mapper.Map<IEnumerable<MovieReadDto>>(movies));
         }
     }
 }
