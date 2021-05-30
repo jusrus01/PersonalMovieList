c84f4a9af68bafb12549c3dfa618de9452621048
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-30 15:52:30 +0300

    Created simple Jwt class to hold some data

---
+++ backend/PersonalMovieListApi/Settings/Jwt.cs
@@ -0,0 +1,10 @@
+namespace PersonalMovieListApi.Settings
+{
+    public class Jwt
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public double DurationInMinutes { get; set; }
+    }
+}
