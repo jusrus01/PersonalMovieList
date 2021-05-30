228de05979fba242596311d2d2ce92d334da5c51
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-30 16:17:43 +0300

    Created constant roles

---
+++ backend/PersonalMovieListApi/Settings/Authorization.cs
@@ -0,0 +1,16 @@
+namespace PersonalMovieListApi.Settings
+{
+    public class Authorization
+    {
+        public enum Roles
+        {
+            Administrator,
+            Moderator,
+            User
+        }
+        public const string default_username = "user";
+        public const string default_email = "user@secureapi.com";
+        public const string default_password = "Pa$$w0rd.";
+        public const Roles default_role = Roles.User;
+    }
+}
