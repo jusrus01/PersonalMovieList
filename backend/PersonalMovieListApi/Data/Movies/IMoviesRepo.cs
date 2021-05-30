ab7225718458dd5f62de21afdb79b44109c210c1
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-30 15:49:56 +0300

    moved

---
+++ backend/PersonalMovieListApi/Data/Movies/IMoviesRepo.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using PersonalMovieListApi.Models;
+
+namespace PersonalMovieListApi.Data
+{
+    public interface IMoviesRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Movie> GetAllMovies();
+        Movie GetMovieById(int id);
+        Movie GetMovieByTitle(string title);
+        void CreateMovie(Movie movie);
+        void DeleteMovie(Movie movie);
+        void UpdateMovie(Movie movie);
+    }
+}
---
+++ backend/PersonalMovieListApi/Data/Movies/SqlMoviesRepo.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using PersonalMovieListApi.Models;
+using System.Linq;
+using System;
+
+namespace PersonalMovieListApi.Data
+{
+    public class SqlMoviesRepo : IMoviesRepo
+    {
+        private readonly MoviesDbContext _context;
+
+        public SqlMoviesRepo(MoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateMovie(Movie movie)
+        {
+            if(movie == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            _context.Movies.Add(movie);
+        }
+
+        public void DeleteMovie(Movie movie) // should it reassign id's after deletion?
+        {
+            if(movie == null)
+            {
+                throw new ArgumentNullException(nameof(movie));
+            }
+
+            _context.Movies.Remove(movie);
+        }
+
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            return _context.Movies.FirstOrDefault(m => m.Id == id);
+        }
+
+        public Movie GetMovieByTitle(string title)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            if(movie == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            _context.Movies.Update(movie);
+        }
+    }
+}
