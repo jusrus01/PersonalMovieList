b818552469807ea6f7923f73c1636f7445c7478d
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-30 23:26:41 +0300

    Created token model

---
+++ backend/PersonalMovieListApi/Models/TokenRequestModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalMovieListApi.Models
+{
+    public class TokenRequestModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
