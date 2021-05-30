1358cf2943947441dfc69a7e16f4abf7e01fccc9
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-30 23:26:23 +0300

    Added and implemented token generation methods

--- backend/PersonalMovieListApi/Data/Users/IUserService.cs
+++ backend/PersonalMovieListApi/Data/Users/IUserService.cs
@@ -6,5 +6,6 @@ namespace PersonalMovieListApi.Data.Users
     public interface IUserService
     {
         Task<string> RegisterAsync(RegisterModel model);
+        Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
     }
 }
--- backend/PersonalMovieListApi/Data/Users/UserService.cs
+++ backend/PersonalMovieListApi/Data/Users/UserService.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using PersonalMovieListApi.Models;
 using PersonalMovieListApi.Settings;
 
@@ -18,6 +25,65 @@ namespace PersonalMovieListApi.Data.Users
             _userManager = userManager;
             _roleManager = roleManager;
             _jwt = jwt.Value;
+        }
+
+        public async Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model)
+        {
+            var authenticationModel = new AuthenticationModel();
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                authenticationModel.IsAuthenticated = false;
+                authenticationModel.Message = $"No Accounts Registered with {model.Email}.";
+                return authenticationModel;
+            }
+
+            if (await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                authenticationModel.IsAuthenticated = true;
+                JwtSecurityToken jwtSecurityToken = await CreateJwtToken(user);
+                authenticationModel.Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+                authenticationModel.Email = user.Email;
+                authenticationModel.UserName = user.UserName;
+                var rolesList = await _userManager.GetRolesAsync(user).ConfigureAwait(false);
+                authenticationModel.Roles = rolesList.ToList();
+                return authenticationModel;
+            }
+            
+            authenticationModel.IsAuthenticated = false;
+            authenticationModel.Message = $"Incorrect Credentials for user {user.Email}.";
+            return authenticationModel;
+        }
+        private async Task<JwtSecurityToken> CreateJwtToken(User user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var roleClaims = new List<Claim>();
+            for (int i = 0; i < roles.Count; i++)
+            {
+                roleClaims.Add(item: new Claim("roles", roles[i]));
+            }
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwt.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwt.Issuer,
+                audience: _jwt.Audience,
+                claims: claims,
+                expires: System.DateTime.UtcNow.AddMinutes(_jwt.DurationInMinutes),
+                signingCredentials: signingCredentials);
+            
+            return jwtSecurityToken;
         }
 
         public async Task<string> RegisterAsync(RegisterModel model)
