dc24fe2e0de047a0ab8a2f5c79c3acf847e572b9
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-30 23:26:58 +0300

    Created authentication model

---
+++ backend/PersonalMovieListApi/Models/AuthenticationModel.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace PersonalMovieListApi.Models
+{
+    public class AuthenticationModel
+    {
+        public string Message { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public List<string> Roles { get; set; }
+        public string Token { get; set; }
+    }
+}
