bc6db15d184da7bb2a427cecea2dcf474cce8669
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-05-30 16:17:17 +0300

    Implemented Register functionality

--- backend/PersonalMovieListApi/Data/Users/IUserService.cs
+++ backend/PersonalMovieListApi/Data/Users/IUserService.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using PersonalMovieListApi.Models;
+
 namespace PersonalMovieListApi.Data.Users
 {
     public interface IUserService
     {
-        
+        Task<string> RegisterAsync(RegisterModel model);
     }
 }
--- backend/PersonalMovieListApi/Data/Users/UserService.cs
+++ backend/PersonalMovieListApi/Data/Users/UserService.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using PersonalMovieListApi.Models;
@@ -17,6 +18,32 @@ namespace PersonalMovieListApi.Data.Users
             _userManager = userManager;
             _roleManager = roleManager;
             _jwt = jwt.Value;
+        }
+
+        public async Task<string> RegisterAsync(RegisterModel model)
+        {
+            var user = new User
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            var userWithSameEmail = await _userManager.FindByEmailAsync(model.Email);
+            if (userWithSameEmail == null)
+            {
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, Authorization.default_role.ToString());                 
+                }
+                return $"User Registered with username {user.UserName}";
+            }
+            else
+            {
+                return $"Email {user.Email } is already registered.";
+            }
         }
     }
 }
